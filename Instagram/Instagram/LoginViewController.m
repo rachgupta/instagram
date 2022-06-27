15e6fd028cdea68bf41a0201032cc2405ecb6467
Rachna Gupta <rachnagupta@fb.com>
2022-06-27 13:39:12 -0700

    added functional login page with segue to blank homefeed page

---
+++ Instagram/Instagram/LoginViewController.m
@@ -0,0 +1,103 @@
+//
+//  LoginViewController.m
+//  Instagram
+//
+//  Created by Rachna Gupta on 6/27/22.
+//
+
+#import "LoginViewController.h"
+#import "Parse/Parse.h"
+
+@interface LoginViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
+
+@end
+
+@implementation LoginViewController
+- (void) checkFields
+{
+    if([self.usernameField.text isEqual:@""])
+    {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"No Username"
+                                                                                   message:@"Please input a Username"
+                                                                            preferredStyle:(UIAlertControllerStyleAlert)];
+        // create an OK action
+        UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK"
+                                                           style:UIAlertActionStyleDefault
+                                                         handler:^(UIAlertAction * _Nonnull action) {
+                                                                 // handle response here.
+                                                         }];
+        // add the OK action to the alert controller
+        [alert addAction:okAction];
+        [self presentViewController:alert animated:YES completion:^{
+            // optional code for what happens after the alert controller has finished presenting
+        }];
+    }
+    if([self.passwordField.text isEqual:@""])
+    {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"No Password"
+                                                                                   message:@"Please input a Password"
+                                                                            preferredStyle:(UIAlertControllerStyleAlert)];
+        // create an OK action
+        UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK"
+                                                           style:UIAlertActionStyleDefault
+                                                         handler:^(UIAlertAction * _Nonnull action) {
+                                                                 // handle response here.
+                                                         }];
+        // add the OK action to the alert controller
+        [alert addAction:okAction];
+        [self presentViewController:alert animated:YES completion:^{
+            // optional code for what happens after the alert controller has finished presenting
+        }];
+    }
+}
+- (IBAction)registerUser:(id)sender {
+    [self checkFields];
+    PFUser *newUser = [PFUser user];
+        
+        // set user properties
+        newUser.username = self.usernameField.text;
+        newUser.password = self.passwordField.text;
+        // call sign up function on the object
+        [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+            if (error != nil) {
+                NSLog(@"Error: %@", error.localizedDescription);
+            } else {
+                NSLog(@"User registered successfully");
+                [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+                // manually segue to logged in view
+            }
+        }];
+}
+- (IBAction)loginUser:(id)sender {
+    [self checkFields];
+    NSString *username = self.usernameField.text;
+    NSString *password = self.passwordField.text;
+        
+    [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+        if (error != nil) {
+            NSLog(@"User log in failed: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User logged in successfully");
+            [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+        }
+    }];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
